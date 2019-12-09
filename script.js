b7f694c363facae8e8ac1716fb7e60f5e3f80a5e
Jose Pereira <jpereira@Joses-MacBook-Pro.local>
2019-12-09 18:08:07 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,27 @@
+const allSquares = [];
+
+function genSquares(){
+    let numValue = 0 ;
+    for (i = 0; i >= 24; i++){
+        numValue = Math.floor(Math.random() * Math.floor(24));
+        allSquares.push(numValue);
+    }
+}
+
+genSquares();
+
+
+const SquareGame = new SquareGame(squares);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let html = '';
+    SquareGame.squares.forEach(piece => {
+      html += `<div class="card" data-card-name="${piece}">`;
+      html += `<div class="back" name="${piece}"></div>`;
+      html += `<div class="front" style="background: url(img/${piece.img}) no-repeat"></div>`;
+      html += `</div>`;
+    });
+
+    // Add all the divs to the HTML
+  document.querySelector('#square').innerHTML = html;
+
---
+++ square.js
@@ -0,0 +1,27 @@
+class SquareGame {
+    constructor(squares){
+      this.square = squares;
+      // add the rest of the class properties here
+      this.pickedSquares = [];
+      this.SquaresMatches = 0;
+    }
+
+
+suffleSquare(){
+
+
+}
+
+
+gameFinished() {
+
+    
+}
+
+
+
+
+
+}
+
+
