ddaf020e337990cefc667effec9eac1f6135a16a
Jose Pereira <jmanuelpereira2@gmail.com>
2019-12-12 14:33:41 -0500

    adding things

---
+++ data.js
@@ -0,0 +1,148 @@
+const allSquares = [];
+
+const names = [
+  {
+    name: "Andromeda",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   {
+    name: "Dwingeloo 1",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   {
+    name: "Centaurus A",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   {
+    name: "Circinus",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Antennae",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Andromeda II",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Barnard",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   
+   {
+    name: "Sombrero",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   
+   {
+    name: "Cigarro",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   
+   {
+    name: "Remolino",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   
+   {
+    name: "Sextans A",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   
+   {
+    name: "M66",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+   
+   {
+    name: "NGC 300",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "los Ojos",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Bode",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Cometa",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Molinete",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+   {
+    name: "Nube de Magallanes",
+    score: -10,
+    enemys: "",
+    planets: ["ARD", "", "" , "", "", ""]
+    
+   },
+
+  ]
--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-const allSquares = [];
-
-const names = [
-  {
-    name: "alien",
-    score: -10
-   },
-   {
-     name: "spaceship",
-     score: 5
-   },
-   {
-     name: "stars",
-     score: 3
-    },
-    {
-      name: 'planets',
-      score: 1
-    } 
-
-  ]
-
 function genSquares(){
     let numValue = 0 ;
-    for (i = 0; i < 36; i++){
-        numValue = Math.floor(Math.random() * 36);
+    for (i = 0; i < names.length; i++){
+        numValue = Math.floor(Math.random() * names.length);
         // allSquares.push(numValue);
         allSquares.push(names[Math.floor(Math.random() * names.length)]);
 
     }
-    console.log(allSquares)
+    // console.log(allSquares)
 }
 
 
@@ -42,10 +20,12 @@ const nuevoGame = new SquareGame(allSquares);
 document.addEventListener("DOMContentLoaded", function(event) { 
     let html = '';
     nuevoGame.square.forEach(id => {
-      console.log(id)
+      
       html += `<div class="square"  name="${id.name}" score = ${id.score}>`;
       html += `<div class="back ${id.name}" name="${id.name}" score = ${id.score}></div>`;
       html += `<div class="front"></div>`;
+      html += `<img src="galaxy.png" alt="${id.name}">`;
+      html += `<p class="text">${id.name}</p>`;
       html += `</div>`;
     });
 
@@ -54,28 +34,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-// Bind the click event of each element to a function
-// document.querySelectorAll('.square').forEach( oneSquare => {
-//     oneSquare.onclick = function() {
-//       console.log("Function works")
-//       oneSquare.classList.add("turned");
-//       const valor = oneSquare.attributes[0].value
-//       console.log(valor)
-//     }
-// })
+console.log(document.querySelectorAll('.square'))
 
-function flipCard() {
-
-  for (let i = 0; i < allSquares.length; i++) {
-    
-    let square = document.querySelector(`body > div.gameTable > div:nth-child(${i + 1})`)
-    
-    square.onClick = function() {
-      console.log(square)
+document.querySelectorAll('.square').forEach( oneSquare => {
+    oneSquare.onclick = function() {
+      console.log("Function works")
+      // oneSquare.classList.add("turned");
+      // const valor = oneSquare.attributes[0].value
+      console.log(valor)
     }
-    // console.log(square)
-  }
+})
 
-}
+// function flipCard() {
+
+//   for (let i = 0; i < allSquares.length; i++) {
+    
+//     let square = document.querySelector(`body > div.gameTable > div:nth-child(${i + 1})`)
+    
+//     square.onClick = function() {
+//       console.log("hello")
+//     }
+//     // console.log(square)
+//   }
+
+// }
 
 
