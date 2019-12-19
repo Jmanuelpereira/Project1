19166101ecfc240368dccd489863ca39d6e616e6
Jose Pereira <jmanuelpereira2@gmail.com>
2019-12-19 10:35:49 -0500

    adding final changes

--- data.js
+++ data.js
@@ -4,35 +4,45 @@ const names = [
   {
     name: "Alfa Romeo",
     price: 350,
-    enemys: "",
-    description: 'This galaxy is no muy conocida',
+    estado: true,
+    numPlanets: 3,
+    description: 'This galaxy is a bit weird, we have little information about it, but many believe it can be an oasis of hidden life, but we don`t have enough information to support that.',
     //suns start
     suns: [
 
       {
-      name: "Canopus",
+      name: "Estelar",
       old: 4567,
-      description: " This sun is so hot but still have some planets TEST with good condition to support life",
+      description: " This sun is so hot but still have some planets with good condition to support life, ",
       price:70,
       //planets start
       planets: [
         {
-        name: "ARD",
-        price: 50,
+        name: "ZRTX-52",
+        price: 100,
         sustainable: true,
-        img: "planet.png"
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
       },
       {
-        name: "ARD",
-        price: 50,
+        name: "ARD-B13",
+        price: 100,
         sustainable: true,
-        img: "planet.png"
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
       },
       {
-        name: "ARD",
-        price: 50,
+        name: "JGP-120",
+        price: 100,
         sustainable: true,
-        img: "planet.png"
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
       },
       
       ]//planet end
@@ -47,23 +57,32 @@ const names = [
         //planets start
         planets: [
           {
-          name: "ARD",
-          price: 50,
-          sustainable: true,
-          img: "planet.png"
-        },
-        {
-          name: "ARD",
-          price: 50,
-          sustainable: true,
-          img: "planet.png"
-        },
-        {
-          name: "ARD",
-          price: 50,
-          sustainable: true,
-          img: "planet.png"
-        },
+            name: "ZRTX-52",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "ARD-B13",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "JGP-120",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
         
         ]//planet end
   
@@ -76,23 +95,32 @@ const names = [
         price:70,
         planets: [
           {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
+            name: "ZRTX-52",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "ARD-B13",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "JGP-120",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
         
         ]//planet end
   
@@ -104,24 +132,33 @@ const names = [
           description: " This sun is so hot but still have some planets with good condition to support life",
           price:70,
           planets: [
-            {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
+        {
+        name: "ZRTX-52",
+        price: 100,
+        sustainable: true,
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
+      },
+      {
+        name: "ARD-B13",
+        price: 100,
+        sustainable: true,
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
+      },
+      {
+        name: "JGP-120",
+        price: 100,
+        sustainable: true,
+        img: "planet.png",
+        description: "",
+        atmosfera: "",
+        size: ""
+      },
           
           ]//planet end
     
@@ -135,37 +172,65 @@ const names = [
 
    {
     name: "Andromeda",
-    price: 50,
-    enemys: "",
-    description: 'Esta otra menos',
+    price: 450,
+    estado: false,
+    numPlanets: 5,
+    description: 'We discovered this galaxy recently but our long distance probe showed us distorted images of planets with possible habitable atmospheres, it is tangent information but the doubt will always remain in you as a pilot.',
     //suns start
     suns: [
 
       {
       name: "Enana Blanca",
       old: 2356,
-      description: " This sun is so hot but still have some planets with good condition to support life",
-      price:70,
+      description: "This white dwarf is almost dead, its energy is almost depleted could provide us with light for a few thousand years but then we would have to go somewhere else if we want to continue with the human species.",
+      price:140,
       //planets start
       planets: [
         {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
-      {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
-      {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
+          name: "ZRTX-52",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "ARD-B13",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "JGP-120",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "JGP-120",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "JGP-120",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
       
       ]//planet end
 
@@ -174,27 +239,36 @@ const names = [
       {
         name: "Alternative",
         old: 4567,
-        description: " This sun is so hot but still have some planets with good condition to support life",
-        price:70,
+        description: "It is an old sun but it has high potential. Its planets meet high expectations of bringing life and with many moons around, it could be an adventure, you can imagine 2 moons at night.",
+        price:170,
         planets: [
           {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
+            name: "ZRTX-52",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "ARD-B13",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "JGP-120",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
         
         ]//planet end
   
@@ -207,23 +281,32 @@ const names = [
           price:70,
           planets: [
             {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
+              name: "ZRTX-52",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
+            {
+              name: "ARD-B13",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
+            {
+              name: "JGP-120",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
           
           ]//planet end
     
@@ -237,9 +320,10 @@ const names = [
 
    {
     name: "Pegasi",
-    price: -10,
-    enemys: "",
-    description: 'Esta podria servir',
+    price: 650,
+    estado: true,
+    numPlanets: 9,
+    description: 'This beautiful galaxy is a mystery, we have never been able to see it completely and reach it would mean spending all our fuel and if we do not find anything safe we ​​must say goodbye to our lives, it is up to you captain.',
     //suns start
     suns: [
 
@@ -251,23 +335,32 @@ const names = [
       //planets start
       planets: [
         {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
-      {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
-      {
-        name: "ARD",
-        price: -50,
-        sustainable: true,
-        img: "planet.png"
-      }, 
+          name: "ZRTX-52",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "ARD-B13",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
+        {
+          name: "JGP-120",
+          price: 100,
+          sustainable: true,
+          img: "planet.png",
+          description: "",
+          atmosfera: "",
+          size: ""
+        },
       
       ]//planet end
 
@@ -280,23 +373,32 @@ const names = [
         price:70,
         planets: [
           {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
-        {
-          name: "ARD",
-          price: -50,
-          sustainable: true,
-          img: "planet.png"
-        }, 
+            name: "ZRTX-52",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "ARD-B13",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
+          {
+            name: "JGP-120",
+            price: 100,
+            sustainable: true,
+            img: "planet.png",
+            description: "",
+            atmosfera: "",
+            size: ""
+          },
         
         ]//planet end
   
@@ -309,23 +411,32 @@ const names = [
           price:70,
           planets: [
             {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
-          {
-            name: "ARD",
-            price: -50,
-            sustainable: true,
-            img: "planet.png"
-          }, 
+              name: "ZRTX-52",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
+            {
+              name: "ARD-B13",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
+            {
+              name: "JGP-120",
+              price: 100,
+              sustainable: true,
+              img: "planet.png",
+              description: "",
+              atmosfera: "",
+              size: ""
+            },
           
           ]//planet end
     
@@ -340,8 +451,9 @@ const names = [
    {
     name: "Messier",
     price: -10,
-    enemys: "",
-    description: 'Esta tal vez es una mierda',
+    estado: "",
+    numPlanets: 3,
+    description: 'This galaxy we know very well however when we were about to land on the last planet something happened with the ship and communication was lost, we could never know if that last planet could be the right one, we would go and find out for ourselves.',
     //suns start
     suns: [
 
@@ -441,9 +553,10 @@ const names = [
 
    {
     name: "Sombrero",
-    price: -10,
-    enemys: "",
-    description: '',
+    price: 410,
+    estado: "",
+    numPlanets: 7,
+    description: 'This galaxy seems to be made by a Mexican, the name gives us the idea that it may be a good thing but we have no real information about this site, we only know that it is a nearby place that we could visit and know in the hope of finding a new home.',
     //suns start
     suns: [
 
@@ -543,9 +656,10 @@ const names = [
 
    {
     name: "Centaurus",
-    price: -10,
-    enemys: "",
-    description: '',
+    price: 750,
+    estado: "",
+    numPlanets: 3,
+    description: 'On earth we always think that this is the best option, it has all the possibilities of being a place for life and to sustain us for years but we never take on the task of sending an exploration ship with living beings, many believe that this is the suitable but will it be?',
     //suns start
     suns: [
 
@@ -641,13 +755,14 @@ const names = [
 
    },
 
-   //galaxy 7b
+   //galaxy 7
    
    {
     name: "Pinwheel",
     price: -10,
-    enemys: "",
-    description: '',
+    estado: "",
+    numPlanets: 3,
+    description: 'The data in the system about this galaxy is corrupt.',
     //suns start
     suns: [
 
--- script.js
+++ script.js
@@ -6,41 +6,62 @@
   let descrip = document.querySelector(".description");
 
   let selectedGalaxy;
-  let iniFuel = 100;
+  let iniFuel = 1000;
   let costFuel = 0;
 
+  
 
   //hover things
 
   let galaxy;
   let sun;
   let planet;
+
+  
   
   document.addEventListener("DOMContentLoaded", function(event) { 
-  
+
+
+    document.querySelector('.start').onclick = function() {
+      
+      
+      
+      let startButton = document.querySelector('.startButtons');
+      startButton.classList.add('hidden-level')
+      
+      
+      // let backButton = document.querySelector('.back2');
+      // backButton.classList.add('show');
+
+      // backButton.onclick = function(){
+
+      //   console.log('going backs')
+        
+      //   let x = window.location;
+
+      //   console.log(x)
+
+      // }
+      
+      
     //creating timer
   
-  document.querySelector('.start').onclick = function() {
-  
-    
-  
-  let startButton = document.querySelector('.start');
-  startButton.classList.add('hidden-level')
-  
-  
-    let timer = 300
+    let timer = 180
   
     setInterval(function(){
+      // start:
       timer--
   
       document.querySelector(".timer").innerHTML = timer;
       if (timer === 0) {
-        return "Game Over"
+        timer = 180;
+        setTimeout(function(){
+          document.location.reload()
+        }, 1000);
+        return alert("You dont have more time, YOU LOSE!")
+      
       }
     }, 1000)
-
-
-
 
 
 //some util functions 
@@ -59,6 +80,7 @@ function genSquares(){
 
 function actualFuel(fuelCost){
 
+
   if(iniFuel >= 0){
   iniFuel -= fuelCost
   return iniFuel
@@ -68,7 +90,7 @@ function actualFuel(fuelCost){
 
 }
 
-document.querySelector(".fuel").innerHTML = actualFuel(iniFuel)
+document.querySelector(".fuel").innerHTML = iniFuel
 
 
 //creating galaxys ramdon
@@ -119,7 +141,7 @@ document.querySelector('.first-level').innerHTML = html;
   function createPlanets(allPlanets) {
     let htmlPlanets = '';
 allPlanets.forEach((planet, id) => {
-  htmlPlanets += `<div class="square"  name="${planet.name}" price = ${planet.price}>`;
+  htmlPlanets += `<div class="square planet"  name="${planet.name}" price = ${planet.price}>`;
   htmlPlanets += `<img src="planet${id}.png" alt="${planet.name}">`;
   htmlPlanets += `<p class="text">${planet.name}</p>`;
   htmlPlanets += `</div>`;
@@ -130,32 +152,39 @@ allPlanets.forEach((planet, id) => {
   thirdLevel.innerHTML = htmlPlanets;
   }
 
-
-//end timer function
-
-
   // obteniendo el click del usuario para el primer level (Galaxias)
   document.querySelectorAll('.galaxy').forEach( oneSquare => {
 
       oneSquare.addEventListener("mouseover", function( event ) { 
 
-
         //  descrip.innerHTML = oneSquare.insertAdjacentHTML
         let selectedGalaxy = oneSquare.getAttribute("name")
         
         let galDes = names.filter(galaxy => 
-          galaxy.name === selectedGalaxy
-          )
-          
+          galaxy.name === selectedGalaxy)
 
-          descrip.innerHTML = `<h4>${galDes[0].description}</h4>`
+        descrip.innerHTML = 
+        
+        
+        `
+
+        <ul>
+
+        <li><h4>${galDes[0].description}</h4></li>
+        <br>
+        <li>Price: ${galDes[0].price} fuel</li>
+        <li>Qty Planets: ${galDes[0].numPlanets}</li>
+        <li></li>
+
+      <ul>
+        
+        
+        
+        `
           
-   
      }) 
 
-
       oneSquare.onclick = function() {
-
         
         selectedGalaxy = oneSquare.getAttribute("name")
         // secondLevel = true;
@@ -178,6 +207,8 @@ allPlanets.forEach((planet, id) => {
 
           callStarts();
 
+          showPlanetsInfo();
+
       }
 
   })
@@ -190,7 +221,7 @@ allPlanets.forEach((planet, id) => {
   //beginning level 2 
 
 
-const callStarts = () => document.querySelectorAll('.star').forEach( start => {
+const callStarts = () => document.querySelectorAll('.star').forEach(( start, id) => {
 
 
   start.addEventListener("mouseover", function( event ) { 
@@ -199,11 +230,9 @@ const callStarts = () => document.querySelectorAll('.star').forEach( start => {
     let sunDes = names.filter(galaxy => 
       galaxy.name === selectedGalaxy
       )
-
-      // console.log(sunDes.suns.description)
+      // console.log(sunDes[0].suns[id].description)
       
-      
-      descrip.innerHTML = `<h4>${sunDes[0].suns[0].description}</h4>`
+      descrip.innerHTML = `<h4>${sunDes[0].suns[id].description}</h4>`
       
 
  }) 
@@ -231,14 +260,23 @@ const callStarts = () => document.querySelectorAll('.star').forEach( start => {
 }); // ending level de suns
 
 
-
-//ADDING INITIAL DATA TO HTML
-
-// let description = document.querySelector('.description');
-
-// description.innerHTML = initialDescription;
+const showPlanetsInfo = () => document.querySelectorAll('.planet').forEach(( start, id) => {
 
 
+  start.addEventListener("mouseover", function( event ) { 
+
+    
+    let sunDes = names.filter(galaxy => 
+      galaxy.name === selectedGalaxy
+      )
+      // console.log(sunDes[0].suns[id].description)
+      
+      // descrip.innerHTML = `<h4>${sunDes[0].suns[0].planets[id].description}</h4>`
+      
+
+ }) 
+
+});
 
 
   
